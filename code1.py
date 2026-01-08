f08bac880f9c67d62afa7088aed9b8ae5099a0a3
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2026-01-08 12:08:23 +0530

    done

--- code1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dict={'name':["aparna","pankaj","sudhir","geeku"],
-      'degree':["MBA","BCA","M.TECH","MBA"],
-      'score':[90,40,80,98]}
-
-df=pd.DataFrame(dict)
-print(df)
---
+++ dv-1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dict={'name':["aparna","pankaj","sudhir","geeku"],
+      'degree':["MBA","BCA","M.TECH","MBA"],
+      'score':[90,40,80,98]}
+
+df=pd.DataFrame(dict)
+print(df)
