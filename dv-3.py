077c6ea2a132d03214c9cf6e29ad3b2e8035b09a
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2026-01-13 11:58:27 +0530

    done

---
+++ dv-3.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+#Step-1: Read the Iris Dataset CSV file
+df=pd.read_csv("iris.csv")
+
+#Step-2: Display First few rows of the dataset
+print("Iris Dataset:\n")
+print(df.head())
+
+#Step-3: Take random samples from the entire dataset:
+print("\nRandom Sample from Dataset:\n")
+print(df.sample(10))
+
+#Step-4: Display maximum value of all numeric attribute:
+print("\nMaximum value of numeric attributes:\n")
+print(df.max(numeric_only=True))
