e3cb4374f83f30bdfcaaad206c7343b5f61e7917
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2026-01-15 11:56:48 +0530

    Done

---
+++ dv-4.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+#Step-1: Read the Iris Dataset CSV file
+df=pd.read_csv("Student_Performance_Analysis.csv")
+
+#Step-2: Display First few rows of the dataset
+print("Student Performance Analysis Dataset:\n")
+print(df.head())
+
+#Step-3: Take random samples from the entire dataset:
+print("\nRandom Sample from Dataset:\n")
+print(df.sample(10))
+
+#Step-4: Display maximum value of all numeric attribute:
+print("\nMaximum value of Marks attributes:\n")
+print(df.max(numeric_only=True))
+
+#Step-5: Display total number of records
+print("\nTotal number of records in dataset:")
+print (len(df))
+
+#Step-6: Count number of records for each distinct class (Subject)
+print("\nNumber of records for each Subject:")
+class_count=df['Subject'].value_counts()
+print(class_count)
+
+#Step-7: Display column_wise mean(numeric columns only)
+print("\nColumn-wise Mean:")
+mean_values=df.mean(numeric_only=True)
